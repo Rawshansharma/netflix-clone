1254ac3c0cd8e1b974d93cf962afe1606972415f
Raushan Sharma01~ <raushankumar7485852533@gmail.com>
2023-05-28 23:23:29 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import './App.css';
 import Homescreen from './screens/HomeScreen';
-import { Routes, Route } from "react-router-dom"
-import Loginscreen from './Loginscreen';
+ 
 
 function App() {
-  const user = null;
+  
   return (
     <div className='app'>
-       {!user ? (<Loginscreen />) : (
-          <Routes>
-          <Route exact path="/" element={ <Homescreen/> } />
-          
-        </Routes>
-       
-       )}
+        <Homescreen/>
      
 
      </div>
